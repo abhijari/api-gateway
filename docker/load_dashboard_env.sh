587ed2ae5826f30b9cb6f2e274eb12ee116a02ef
abhijari <abhijariwala13@gmail.com>
2026-01-21 01:33:23 +0530

    Update PREFIX variable in load_dashboard_env.sh to point to the dashboard environment

--- docker/load_dashboard_env.sh
+++ docker/load_dashboard_env.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-PREFIX="/api-gateway/${ENV}"
+PREFIX="/dashboard/${ENV}"
 
 export NODE_ENV=$(aws ssm get-parameter --name "$PREFIX/NODE_ENV" --query Parameter.Value --output text)
 export GATEWAY_API_URL=$(aws ssm get-parameter --name "$PREFIX/GATEWAY_API_URL" --query Parameter.Value --output text)
